66db947bdacb8d8af20e6b8eef4a52cc95fe8be7
MarcoAraujoNeves <marcoaraujoneves@gmail.com>
2018-07-21 09:25:11 -0300

    Alterações na estrutura do projeto, finalização do layout do menu

---
+++ js/Servicos.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    /* Ao entrar na página de serviços, o link de Serviços no menu será marcado como ativo, destacando a posição */
+    $('#link_servicos').addClass("link_ativo");
+});
