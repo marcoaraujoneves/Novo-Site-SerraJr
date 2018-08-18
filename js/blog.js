de8ef35672724551fcc21dc77b14b5f6713d5442
ANK-dev <arielnogueirak@gmail.com>
2018-08-18 17:16:09 -0300

    Menu e página do blog remodeladas

--- js/blog.js
+++ js/blog.js
@@ -4,13 +4,13 @@ $(document).ready(function(){
     function checaLargura(){
         var larguraTela = $window.innerWidth();
         if(window.innerWidth < 768){
-            $('.container-header').css('height','100px');
-            $('.headerBlog').css('background-image','none');
+            $('#container_header').css('height','100px');
+            $('#headerBlog').css('background-image','none');
             $('#titulo').removeClass('display-1');
             $('#subtitulo').css('font-size', '1em');
         } else {
-            $('.container-header').css('height','300px');
-            $('.headerBlog').css('background-image','url("img/Blog/hero.png")');
+            $('#container_header').css('height','300px');
+            $('#headerBlog').css('background-image','url("img/Blog/hero.png")');
             $('#titulo').addClass('display-1');
             $('#subtitulo').css('font-size','');
         }
