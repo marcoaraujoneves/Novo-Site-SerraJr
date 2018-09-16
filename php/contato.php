329429e30cee8b4eb8b352ce49baf6e205f04ce2
MarcoAraujoNeves <marcoaraujoneves@gmail.com>
2018-09-16 20:42:09 -0300

    .

--- php/contato.php
+++ php/contato.php
@@ -8,15 +8,16 @@
     $telRemetente = $_POST['numero'];
     $msgRemetente = $_POST['msg'];
 
-    $msgHTML = '<p> Mensagem recebida através do Site </p>
+    $msgHTML = '<html><body>
+                <p> Mensagem recebida através do Site </p>
                 <h3> Nome: '.$nomeRemetente.' </h3>
                 <h3> E-mail: '.$emailRemetente.'  </h3>
                 <h3> Telefone: '.$telRemetente.'  </h3>
                 <p><i> Mensagem: '.$msgRemetente.' </i></p>
-                <hr>';
+                <hr></body></html>';
 
-    $headers = "MIME-Version: 1.1\r\n";
-    $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+    $headers = "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
     $headers .= "From: ".$emailSender."\r\n";
     $headers .= "Return-Path: ".$emailSender."\r\n";
     $headers .= "Reply-To: ".$emailRemetente."\r\n";
